779d9d7bf7f5cca3a89beea47f019508df8fb7b6
Jakub Kowalski <support@chop-chop.org>
2022-01-31 16:35:44 +0100

    🥷Added: Header/Footer (unfinished) components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
-import styles from './Header.module.scss'; // Import css modules stylesheet as styles
-import '../../styles/styles.scss'; // Import regular stylesheet
+import logo from '../../logo.svg';
+import style from './Header.module.scss';
+import '../../styles/styles.scss';
 
 class Header extends Component {
   render() {
-    // reference as a js object
-    return <button className={styles.test}>Hello There!</button>;
+    return (
+      <header className={style.mainHeader}>
+        <div className={style.mainHeader__logoBox}>
+          <img src={logo} className={style.mainHeader__logo} alt="logo" />
+        </div>
+        <div className={style.mainHeader__menuBox}>
+          <ul>
+            <li>There Would be NAVIGATION</li>
+          </ul>
+        </div>
+      </header>
+    );
   }
 }
 
